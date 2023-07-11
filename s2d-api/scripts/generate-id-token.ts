9289e949bf4905c2fdbd510e9cb9ae4e026689b6
dgop92 <inevaup@gmail.com>
2023-07-10 19:38:35 -0500

    feat: add generate-id-token

---
+++ s2d-api/scripts/generate-id-token.ts
@@ -0,0 +1,65 @@
+import { getAuth } from "firebase-admin/auth";
+import { initializeApp, applicationDefault } from "firebase-admin/app";
+import axios from "axios";
+
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY || "";
+
+function createUrl(useEmulator: boolean, apiKey: string) {
+  if (useEmulator) {
+    return `http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${apiKey}`;
+  } else {
+    return `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${apiKey}`;
+  }
+}
+
+const createIdTokenfromCustomToken = async (
+  customToken: string,
+  url: string
+) => {
+  try {
+    const res = await axios({
+      url: url,
+      method: "post",
+      data: {
+        token: customToken,
+        returnSecureToken: true,
+      },
+    });
+
+    return res.data.idToken as string;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+async function genAuthToken(): Promise<void> {
+  console.log("initializing firebase app");
+  const firebaseApp = initializeApp({
+    credential: applicationDefault(),
+  });
+  const authFirebaseClient = getAuth(firebaseApp);
+
+  const myArgs = process.argv.slice(2);
+  const uuid: string | undefined = myArgs[0];
+  const useEmulator: boolean = myArgs[1] === "true";
+
+  if (!uuid) {
+    console.log("Please provide a uuid as the first argument.");
+    return;
+  }
+
+  if (FIREBASE_API_KEY === "") {
+    console.log(
+      "Please provide a FIREBASE_API_KEY as an environment variable."
+    );
+    return;
+  }
+
+  const token = await authFirebaseClient.createCustomToken(uuid);
+  const url = createUrl(useEmulator, FIREBASE_API_KEY);
+  const idToken = await createIdTokenfromCustomToken(token, url);
+
+  console.log(idToken);
+}
+
+genAuthToken();
